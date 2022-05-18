c702db8a73bfeac38c6fbdb6796bbf51c51f4d31
zach.renwick <zach.renwick@gmail.com>
2022-05-18 05:56:00 +0000

    "Initial commit"

---
+++ schema/Customers.js
@@ -0,0 +1,46 @@
+cube(`Customers`, {
+  sql: `SELECT * FROM dbt_zach.customers`,
+  
+  preAggregations: {
+    // Pre-Aggregations definitions go here
+    // Learn more here: https://cube.dev/docs/caching/pre-aggregations/getting-started  
+  },
+  
+  joins: {
+    
+  },
+  
+  measures: {
+    count: {
+      type: `count`,
+      drillMembers: [firstName, lastName]
+    }
+  },
+  
+  dimensions: {
+    firstName: {
+      sql: `first_name`,
+      type: `string`
+    },
+    
+    lastName: {
+      sql: `last_name`,
+      type: `string`
+    },
+    
+    customerLifetimeValue: {
+      sql: `customer_lifetime_value`,
+      type: `string`
+    },
+    
+    firstOrder: {
+      sql: `first_order`,
+      type: `time`
+    },
+    
+    mostRecentOrder: {
+      sql: `most_recent_order`,
+      type: `time`
+    }
+  }
+});
---
+++ schema/Orders.js
@@ -0,0 +1,59 @@
+cube(`Orders`, {
+  sql: `SELECT * FROM dbt_zach.orders`,
+  
+  preAggregations: {
+    // Pre-Aggregations definitions go here
+    // Learn more here: https://cube.dev/docs/caching/pre-aggregations/getting-started  
+  },
+  
+  joins: {
+    Customers: {
+      sql: `${CUBE}.customer_id = ${Customers}.customer_id`,
+      relationship: `belongsTo`
+    }
+  },
+  
+  measures: {
+    count: {
+      type: `count`,
+      drillMembers: [orderDate]
+    }
+  },
+  
+  dimensions: {
+    status: {
+      sql: `status`,
+      type: `string`
+    },
+    
+    creditCardAmount: {
+      sql: `credit_card_amount`,
+      type: `string`
+    },
+    
+    couponAmount: {
+      sql: `coupon_amount`,
+      type: `string`
+    },
+    
+    bankTransferAmount: {
+      sql: `bank_transfer_amount`,
+      type: `string`
+    },
+    
+    giftCardAmount: {
+      sql: `gift_card_amount`,
+      type: `string`
+    },
+    
+    amount: {
+      sql: `amount`,
+      type: `string`
+    },
+    
+    orderDate: {
+      sql: `order_date`,
+      type: `time`
+    }
+  }
+});
